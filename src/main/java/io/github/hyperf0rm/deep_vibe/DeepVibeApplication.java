425ea7c955a44ee039183f290e5bcf2cc53fac28
hyperf0rm <drk2701@gmail.com>
2025-12-27 00:37:20 +0300

    Initial commit

---
+++ src/main/java/io/github/hyperf0rm/deep_vibe/DeepVibeApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyperf0rm.deep_vibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepVibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hyperf0rm/deep_vibe/DeepVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyperf0rm.deep_vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
