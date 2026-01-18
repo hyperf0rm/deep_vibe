4118c5a46d18d6bb2a766705e827204845a6542f
hyperf0rm <drk2701@gmail.com>
2026-01-18 21:30:38 +0300

    fix: make DTOs ignore unknown properties

--- src/main/java/io/github/hyperf0rm/deep_vibe/music/dto/PreviewUrlsResponse.java
+++ src/main/java/io/github/hyperf0rm/deep_vibe/music/dto/PreviewUrlsResponse.java
@@ -1,9 +1,13 @@
 package io.github.hyperf0rm.deep_vibe.music.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PreviewUrlsResponse(String resultCount, List<Result> results) {
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Result(String artistName, String trackName, String previewUrl) { }
 
 }
--- src/main/java/io/github/hyperf0rm/deep_vibe/user/LastFmUserResponse.java
+++ src/main/java/io/github/hyperf0rm/deep_vibe/user/LastFmUserResponse.java
@@ -1,7 +1,11 @@
 package io.github.hyperf0rm.deep_vibe.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LastFmUserResponse(LastFmUser user) {
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record LastFmUser(String name) { }
 
 }
