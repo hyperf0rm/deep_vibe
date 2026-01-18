d23806c6f246585b0d22a550b4b456949f5987c9
hyperf0rm <drk2701@gmail.com>
2026-01-18 21:32:38 +0300

    refactor: delete unused classes

--- src/main/java/io/github/hyperf0rm/deep_vibe/user/UserCreateRequest.java
+++
@@ -1,5 +0,0 @@
-package io.github.hyperf0rm.deep_vibe.user;
-
-public record UserCreateRequest(
-        String lastfmUsername
-) { }
--- src/main/java/io/github/hyperf0rm/deep_vibe/user/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package io.github.hyperf0rm.deep_vibe.user;
-
-import java.time.Instant;
-
-public record UserResponse(
-        Integer id,
-        String lastfmUsername,
-        Instant createdAt
-) { }
