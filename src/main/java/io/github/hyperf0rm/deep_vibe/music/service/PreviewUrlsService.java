c8fadbfd48c578d6573c98565e0479b744961ca6
hyperf0rm <drk2701@gmail.com>
2026-01-21 22:25:57 +0300

    fix: handle PreviewUrlService API errors

--- src/main/java/io/github/hyperf0rm/deep_vibe/music/service/PreviewUrlsService.java
+++ src/main/java/io/github/hyperf0rm/deep_vibe/music/service/PreviewUrlsService.java
@@ -1,10 +1,12 @@
 package io.github.hyperf0rm.deep_vibe.music.service;
 
+import io.github.hyperf0rm.deep_vibe.exception.ExternalAPIException;
 import io.github.hyperf0rm.deep_vibe.music.dto.PreviewUrlsResponse;
 import io.github.hyperf0rm.deep_vibe.music.entity.Track;
 import io.github.hyperf0rm.deep_vibe.music.entity.TrackQueueStatus;
 import io.github.hyperf0rm.deep_vibe.music.repository.TrackRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -45,50 +47,74 @@ public class PreviewUrlsService {
 
                 log.info("getting url for track: {}; termParam: {}", fullName, termParam);
 
-                PreviewUrlsResponse response = restClient.get()
-                        .uri(uriBuilder -> uriBuilder
-                                .scheme("https")
-                                .host("itunes.apple.com")
-                                .path("/search")
-                                .queryParam("term", termParam)
-                                .queryParam("media", "music")
-                                .queryParam("entity", "song")
-                                .queryParam("limit", "200")
-                                .build())
-                        .retrieve()
-                        .body(PreviewUrlsResponse.class);
-                if (response == null || response.results() == null || response.results().isEmpty()) {
-                    log.warn("Not found preview url for track: {}", fullName);
-                    track.setStatus(TrackQueueStatus.FAILED);
-                    trackRepository.save(track);
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        log.error("Error during thread.sleep", e);
-                    }
-                }
-                else {
-                    String previewUrl = response.results().getFirst().previewUrl();
-                    log.info("Found default preview url for track {}", fullName);
-
-                    for (PreviewUrlsResponse.Result result : response.results()) {
-                        if (result.artistName().equalsIgnoreCase(artistName)
-                                && result.trackName().equalsIgnoreCase(trackName)) {
-                            previewUrl = result.previewUrl();
-                            log.info("Found preview url for track {} - {}: {}",
-                                    result.artistName(), result.trackName(), previewUrl
-                            );
-                            break;
+                try {
+                    PreviewUrlsResponse response = restClient.get()
+                            .uri(uriBuilder -> uriBuilder
+                                    .scheme("https")
+                                    .host("itunes.apple.com")
+                                    .path("/search")
+                                    .queryParam("term", termParam)
+                                    .queryParam("media", "music")
+                                    .queryParam("entity", "song")
+                                    .queryParam("limit", "200")
+                                    .build())
+                            .exchange((request, resp) -> {
+                                int statusCode = resp.getStatusCode().value();
+                                if (statusCode == 429) {
+                                    log.error("Rate limit for preview url service exceeded");
+                                    throw new ExternalAPIException("Rate limit for preview url service exceeded", 429);
+                                } else if (resp.getStatusCode().is5xxServerError()) {
+                                    throw new ExternalAPIException("iTunes Server Error: " + statusCode, statusCode);
+                                }
+                                else if (resp.getStatusCode().is2xxSuccessful()) {
+                                    return resp.bodyTo(PreviewUrlsResponse.class);
+                                } else {
+                                    return null;
+                                }
+                            });
+                    if (response == null || response.results() == null || response.results().isEmpty()) {
+                        log.warn("Not found preview url for track: {}", fullName);
+                        track.setStatus(TrackQueueStatus.FAILED);
+                        trackRepository.save(track);
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            log.error("Error during thread.sleep", e);
                         }
                     }
-                    track.setPreviewUrl(previewUrl);
-                    trackRepository.save(track);
-                    try {
-                        Thread.sleep(3000);
+                    else {
+                        String previewUrl = response.results().getFirst().previewUrl();
+                        log.info("Found default preview url for track {}", fullName);
+
+                        for (PreviewUrlsResponse.Result result : response.results()) {
+                            if (result.artistName().equalsIgnoreCase(artistName)
+                                    && result.trackName().equalsIgnoreCase(trackName)) {
+                                previewUrl = result.previewUrl();
+                                log.info("Found preview url for track {} - {}: {}",
+                                        result.artistName(), result.trackName(), previewUrl
+                                );
+                                break;
+                            }
+                        }
+                        track.setPreviewUrl(previewUrl);
+                        trackRepository.save(track);
+                    }
+
+                    Thread.sleep(3000);
+
                     } catch (InterruptedException e) {
                         log.error("Error during thread.sleep", e);
+                    } catch (ExternalAPIException e) {
+                        log.error("iTunes 429 Rate Limit hit! Stopping batch processing.");
+                        try {
+                            Thread.sleep(60000);
+                        } catch (InterruptedException err) {
+                            log.error("Error during thread.sleep", err);
+                        }
+                        break;
+                    } catch (Exception e) {
+                        log.error("Unexpected error for track {}: {}", fullName, e.getMessage());
                     }
-                }
             }
         }
     }
